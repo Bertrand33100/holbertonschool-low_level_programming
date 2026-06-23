355f77b3abbee5c4f62b624e66b5973cdedca424
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-06-23 15:19:00 +0200

    Task 2: print alphabet in lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a random number
+ * main - Prints the alphabet in lowercase
  * Return: Always 0
  */
 int main(void)
 {
-	int n;
-	int last_digit;
+	char c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-	last_digit = n % 10;
-
-	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n",
-		       n, last_digit);
-	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n",
-		       n, last_digit);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-		       n, last_digit);
+	putchar('\n');
 
 	return (0);
 }
