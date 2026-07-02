1a0c9f63df51551c527e66dcccd129dd2b4a444d
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-02 21:36:15 +0200

    Implement addition operation

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Simple calculator menu
+ * main - Simple calculator
+ *
+ * Description: This calculator uses decimal division.
  *
  * Return: Always 0
  */
 int main(void)
 {
 	int choice;
+	int a;
+	int b;
 
 	do {
 		printf("Simple Calculator\n");
@@ -19,13 +23,51 @@ int main(void)
 		printf("Choice: ");
 
 		if (scanf("%d", &choice) != 1)
-		{
 			return (1);
-		}
 
 		if (choice < 0 || choice > 4)
 		{
 			printf("Invalid choice\n");
+		}
+		else if (choice == 1)
+		{
+			printf("A: ");
+			scanf("%d", &a);
+			printf("B: ");
+			scanf("%d", &b);
+			printf("Result: %d\n", a + b);
+		}
+		else if (choice == 2)
+		{
+			printf("A: ");
+			scanf("%d", &a);
+			printf("B: ");
+			scanf("%d", &b);
+			printf("Result: %d\n", a - b);
+		}
+		else if (choice == 3)
+		{
+			printf("A: ");
+			scanf("%d", &a);
+			printf("B: ");
+			scanf("%d", &b);
+			printf("Result: %d\n", a * b);
+		}
+		else if (choice == 4)
+		{
+			printf("A: ");
+			scanf("%d", &a);
+			printf("B: ");
+			scanf("%d", &b);
+
+			if (b == 0)
+			{
+				printf("Error: division by zero\n");
+			}
+			else
+			{
+				printf("Result: %.1f\n", (double)a / b);
+			}
 		}
 	} while (choice != 0);
 
