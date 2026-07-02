5b8531a36f3de2e4e8d87f12f3d909f089e9f6c1
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-02 21:18:03 +0200

    Create calculator program skeleton

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
-	
-	int main(void)
-{
-	printf("welcome\n");
-	return 0;
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int choice;
+
+	printf("Simple Calculator\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice: ");
+
+	scanf("%d", &choice);
+
+	if (choice == 0)
+	{
+		printf("Bye!\n");
+	}
+
+	return (0);
 }
