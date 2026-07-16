63f239b3bdd95f7e763d97b5c16442789a575c0b
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-16 11:30:57 +0200

    Prototype

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,8 +3,20 @@
 
 #include <stdarg.h>
 
+/**
+ * struct printer - printer structure
+ * @symbol: format symbol
+ * @print: function pointer
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list);
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
