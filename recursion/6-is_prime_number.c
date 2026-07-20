58a36aa660fee619796a2585c2afbb879dba5eaa
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-20 13:35:53 +0200

    6. Inception. Is it possible?

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * prime_helper - checks if a number is prime
+ * @n: number to check
+ * @i: current divisor
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int prime_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	return (prime_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - checks if an integer is a prime number
+ * @n: number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+
+	return (prime_helper(n, 2));
+}
