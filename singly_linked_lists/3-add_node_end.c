f674acb5edc5ce79ca87e6eeceb17ec5ef38798f
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-22 22:12:27 +0200

    3. Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,59 +2,39 @@
 
 /**
  * add_node_end - Ajoute un nouveau nœud à la fin d'une liste chaînée
- * @head: Adresse du pointeur vers le premier nœud
- * @str: Chaîne de caractères à copier dans le nouveau nœud
+ * @head: Adresse du premier nœud
+ * @str: Chaîne à copier
  *
- * Return: Retourne l'adresse du nouveau nœud ou NULL en cas d'échec
+ * Return: Adresse du nouveau nœud ou NULL en cas d'échec
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *temp;
-	unsigned int len;
+	list_t *new_node, *temp;
+	unsigned int len = 0;
 
-	/* Vérifie que les paramètres sont valides */
-	if (head == NULL || str == NULL)
-		return (NULL);
-
-	/* Alloue la mémoire pour le nouveau nœud */
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+	if (new_node == NULL || head == NULL || str == NULL)
+		return (free(new_node), NULL);
 
-	/* Duplique la chaîne de caractères */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+		return (free(new_node), NULL);
 
-	/* Calcule la longueur de la chaîne */
-	len = 0;
 	while (str[len] != '\0')
 		len++;
-
 	new_node->len = len;
-
-	/* Le nouveau nœud sera le dernier */
 	new_node->next = NULL;
 
-	/* Si la liste est vide, le nouveau nœud devient la tête */
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Parcourt la liste jusqu'au dernier nœud */
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
 
-	/* Relie le dernier nœud au nouveau */
 	temp->next = new_node;
-
-	/* Retourne l'adresse du nouveau nœud */
 	return (new_node);
 }
