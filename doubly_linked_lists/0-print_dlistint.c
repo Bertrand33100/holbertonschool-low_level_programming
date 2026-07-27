7ef0dc8c067465b2bdde91caf11a0d5093e8e8af
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-27 09:52:34 +0200

    0. Print list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
@@ -8,4 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
