1dcfd413a0e432e991dc3b0d54ad6bdec2ba1ce7
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-07-30 12:52:16 +0200

    Update

--- secure_data_handling/store.c
+++ secure_data_handling/store.c
@@ -10,12 +10,16 @@ void store_init(store_t *st)
 
 static node_t *node_create(session_t *s)
 {
-	node_t *n = (node_t *)malloc(sizeof(*n));
+	node_t *n;
+
+	n = malloc(sizeof(*n));
 	if (!n)
-		return NULL;
+		return (NULL);
+
 	n->sess = s;
 	n->next = NULL;
-	return n;
+
+	return (n);
 }
 
 int store_add(store_t *st, session_t *s)
@@ -23,23 +27,25 @@ int store_add(store_t *st, session_t *s)
 	node_t *n, *cur;
 
 	if (!st || !s || !s->id)
-		return 0;
+		return (0);
 
 	cur = st->head;
-	while (cur) {
-		if (cur->sess && cur->sess->id && strcmp(cur->sess->id, s->id) == 0)
-			return 0;
+	while (cur)
+	{
+		if (cur->sess && cur->sess->id &&
+			strcmp(cur->sess->id, s->id) == 0)
+			return (0);
 		cur = cur->next;
 	}
 
 	n = node_create(s);
-	if (!n) {
-		return 0;
-	}
+	if (!n)
+		return (0);
 
 	n->next = st->head;
 	st->head = n;
-	return 1;
+
+	return (1);
 }
 
 session_t *store_get(store_t *st, const char *id)
@@ -47,15 +53,19 @@ session_t *store_get(store_t *st, const char *id)
 	node_t *cur;
 
 	if (!st || !id)
-		return NULL;
+		return (NULL);
 
 	cur = st->head;
-	while (cur) {
-		if (cur->sess && cur->sess->id && strcmp(cur->sess->id, id) == 0)
-			return cur->sess;
+	while (cur)
+	{
+		if (cur->sess && cur->sess->id &&
+			strcmp(cur->sess->id, id) == 0)
+			return (cur->sess);
+
 		cur = cur->next;
 	}
-	return NULL;
+
+	return (NULL);
 }
 
 int store_delete(store_t *st, const char *id, session_t **out)
@@ -63,13 +73,16 @@ int store_delete(store_t *st, const char *id, session_t **out)
 	node_t *cur, *prev;
 
 	if (!st || !id)
-		return 0;
+		return (0);
 
 	prev = NULL;
 	cur = st->head;
 
-	while (cur) {
-		if (cur->sess && cur->sess->id && strcmp(cur->sess->id, id) == 0) {
+	while (cur)
+	{
+		if (cur->sess && cur->sess->id &&
+			strcmp(cur->sess->id, id) == 0)
+		{
 			if (prev)
 				prev->next = cur->next;
 			else
@@ -77,16 +90,18 @@ int store_delete(store_t *st, const char *id, session_t **out)
 
 			if (out)
 				*out = cur->sess;
+			else
+				session_destroy(cur->sess);
 
-			session_destroy(cur->sess);
 			free(cur);
-			return 1;
+			return (1);
 		}
+
 		prev = cur;
 		cur = cur->next;
 	}
 
-	return 0;
+	return (0);
 }
 
 void store_destroy(store_t *st)
@@ -97,14 +112,15 @@ void store_destroy(store_t *st)
 		return;
 
 	cur = st->head;
-	while (cur) {
+	while (cur)
+	{
 		next = cur->next;
 
 		session_destroy(cur->sess);
-
 		free(cur);
 
 		cur = next;
 	}
+
 	st->head = NULL;
 }
